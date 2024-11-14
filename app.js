80dbbbee9b0a2ae01c79b5e31716d2191b44be7b
HilarioNengareJr <hnengare@gmail.com>
2024-11-14 11:24:44 +0200

    structure: implement folder structure

--- app.js
+++ app.js
@@ -4,4 +4,46 @@
  * node modules
  */
 
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 // initial express app
+const express = require('express');
+const app = express();
+
+/**
+ * EJS setting
+ */
+
+app.set('view engine', 'ejs');
+
+/**
+ * setting static directory
+ */
+
+app.use(express.static(`${__dirname}/public`));
+
+
+/**
+ * enable cors and cookie parser
+ */
+
+app.use(cors()).use(cookieParser());
+
+
+
+/**
+ * login page
+ */
+
+app.get('/login', (req, res) => {
+    res.send('Login');
+});
+
+/**
+ * 
+ */
+
+app.listen (5000, () => {
+    console.log(`Server listening at http://localhost:5000`);
+})
---
+++ src/routes/login.route.js
@@ -0,0 +1,12 @@
+/**
+ * @license Apache-2.0
+ * @copyright Hilario Junior Nengare 2024
+ */
+
+'use strict'
+
+/**
+ * node modules
+ */
+
+const router = require('express').Router();
