e21167c111f1f8f8a2e7efd869704e6739fa6bc8
HilarioNengareJr <hnengare@gmail.com>
2024-11-14 11:45:23 +0200

    chore: implement login route and ejs

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -10,3 +10,13 @@
  */
 
 const router = require('express').Router();
+
+/**
+ * custom modules
+ */
+
+const { login } = require('../controllers/login.controller');
+
+router.get('/', login);
+
+module.exports = router;
