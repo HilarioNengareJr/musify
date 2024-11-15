a2573ab697c50aa2ec4f3b47012cb4dbbcb6ee64
HilarioNengareJr <hnengare@gmail.com>
2024-11-15 11:13:38 +0200

    chore: implement config file

---
+++ src/config/api.config.js
@@ -0,0 +1,19 @@
+/**
+ * @license Apache-2.0
+ * @copyright Hilario Junior Nengare 2024
+ */
+
+'use strict'
+
+/**
+ * node modules
+ */
+
+require('dotenv').config();
+
+// web api base address
+const BASE_URL = 'https://api.spotify.com/V1';
+
+// spotify token base address
+const TOKEN_BASE_URL = 'https://accounts.spotify.com/api/token';
+
