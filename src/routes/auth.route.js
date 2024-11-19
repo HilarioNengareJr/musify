f580654de2369cd7d92d1b399b5146b7525b4ebe
HilarioNengareJr <hnengare@gmail.com>
2024-11-19 22:31:16 +0300

    refactor: simplify auth route path to root

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -18,7 +18,7 @@ const router = require('express').Router();
 const { auth, callback } = require('../controllers/auth.controller');
 const { refreshToken } = require('../controllers/refresh_token.controller');
 
-router.get('/auth', auth);
+router.get('/', auth);
 
 router.get('/callback', callback);
 
